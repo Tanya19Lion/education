9b11e6bd85c82ec025d8e9ad99ac265841ad0ac0
Tanya19Lion <tanyatriteknitskaya@gmail.com>
2020-03-01 16:22:59 +0200

    added sections slider and about

---
+++ app/js/main.js
@@ -0,0 +1,13 @@
+$(function(){
+ 
+    $(".main_slider_wrapper").slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        nextArrow: '<button type="button" class="next"></button>',
+        prevArrow: '<button type="button" class="prev"></button>',
+        infinite: false
+        // centerMode: true,
+        // focusOnSelect: true,
+    });
+
+});
