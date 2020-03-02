dcf07c47d0297b8a2b1e8be7854df59d4d3f1780
Tanya19Lion <tanyatriteknitskaya@gmail.com>
2020-03-02 15:26:48 +0200

    added news section

--- app/js/main.js
+++ app/js/main.js
@@ -6,21 +6,35 @@ $(function(){
         nextArrow: '<button type="button" class="next"></button>',
         prevArrow: '<button type="button" class="prev"></button>',
         infinite: false
-        // centerMode: true,
-        // focusOnSelect: true,
     });
 
-    $(".rate_star").rateYo({
+    $(".news_inner_slider_wrapper").slick({
+      slidesToShow: 2,
+      slidesToScroll: 1,
+      dots: true,
+      arrows: false
+  });
+
+  $(".instructors_box_name").on("click", function(e){
+    e.preventDefault();
+    $(this).toggleClass("rotate").next().slideToggle();
+    $(".instructors_box_name"
+    ).not(this).removeClass("rotate").next().slideUp();
+  });
+
+  $(".rate_star").rateYo({
         rating: 2,
         readOnly: true,
         starWidth: "18px"
-      });
+    });
 
-      $(".rate_star_five").rateYo({
+    $(".rate_star_five").rateYo({
         rating: 5,
         readOnly: true,
         starWidth: "18px"
-      });
+    });
+
 
       let mixer = mixitup(".gallery_inner_items");
 });
+
